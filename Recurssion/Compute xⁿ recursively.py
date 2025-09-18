8b2d0af8f984e60411eb0bd04abfde4aa77580ce
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2025-09-18 18:53:02 +0530

    Update Compute xⁿ recursively.py

--- Recurssion/Compute xⁿ recursively.py
+++ Recurssion/Compute xⁿ recursively.py
@@ -4,4 +4,15 @@ def power(x,n):
     
     return x ** n
 
+
 print(power(2,2))
+
+# or
+
+def power_v2(x,n):
+    if n == 0:
+        return 1
+    
+    return x * power(x,n-1)
+
+print(power_v2(2,2))
