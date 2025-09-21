b4333e143a21c2a5fe39225188744c332f09eb0b
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2025-09-21 18:25:06 +0530

    Add files via upload

---
+++ 03_Strings/03_Reverse a String.py
@@ -0,0 +1,12 @@
+def Reverse_String(string):
+
+    for i in range(len(string)-1,-1,-1):
+        print(string[i],end="")
+
+Reverse_String("raja")
+
+def reverse_str(string1):
+
+    return string1[::-1]
+
+print(reverse_str("raja\n"))
