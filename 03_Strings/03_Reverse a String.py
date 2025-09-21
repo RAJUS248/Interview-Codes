a4ac3149ad72e6f13e85056814527860394f890c
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2025-09-21 18:31:14 +0530

    Update 03_Reverse a String.py

--- 03_Strings/03_Reverse a String.py
+++ 03_Strings/03_Reverse a String.py
@@ -9,4 +9,21 @@ def reverse_str(string1):
 
     return string1[::-1]
 
+
 print(reverse_str("raja\n"))
+
+
+# using list 
+def reverse_by_swap(string):
+    start = 0
+    end = len(string)-1
+
+    while start < end:
+        string[start],string[end] = string[end],string[start]
+        start += 1
+        end -= 1
+
+    print("".join(string))
+
+string = "raja"
+reverse_by_swap(list(string))
