a1e699d24ca7d30f25ee3e8065b0cc1c5db2c2d9
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2025-09-28 09:04:57 +0530

    Add files via upload

---
+++ 05_Dictionory/06_First Non-Repeating Element in a List.py
@@ -0,0 +1,38 @@
+def first_non_rep(arr):
+    for i in range(len(arr)):
+        is_unique = True
+        for j in range(len(arr)):
+            if i != j and arr[i] == arr[j]:
+                is_unique = False
+                break
+        if is_unique:
+            return arr[i]
+
+arr = [9, 4, 9, 6, 7, 4]
+print(first_non_rep(arr))
+
+
+def first_non_rep_v2(arr):
+    seen = {}
+
+    for num in arr:
+        if num in seen:
+            seen[num] += 1
+
+        else:
+            seen[num] = 1
+
+    for num in arr:
+        if seen[num] == 1:
+            return f"the key value is {num} and the count is {seen[num]}"
+
+
+    """
+    for key,value in (seen.items()):
+        if value == 1:
+            return f"the key is {key} and the count is {value}"
+    """
+
+arr = [9, 4, 9, 6, 7, 4]
+print(first_non_rep_v2(arr))
+
