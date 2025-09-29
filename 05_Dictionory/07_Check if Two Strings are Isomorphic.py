9bc127ea4fc0ee2623bcb496182fcaceb00c6698
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2025-09-29 14:03:58 +0530

    Add files via upload

---
+++ 05_Dictionory/07_Check if Two Strings are Isomorphic.py
@@ -0,0 +1,24 @@
+def is_isomorphic(string1,string2):
+    
+    if len(string1) != len(string2):
+        return False
+    
+    s1 = {}
+    s2 = {}
+
+    for c1,c2 in zip(string1,string2):
+        if c1 in s1 and s1[c1] != c2:
+            return False
+
+        if c2 in s2 and s2[c2] != c1:
+            return False
+        
+        s1[c1] = c2
+        s2[c2] = c1
+
+    return "true"
+        
+string1 = "badc"
+string2 = "baba"
+
+print(is_isomorphic(string1,string2))
