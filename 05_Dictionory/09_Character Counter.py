2f00386f4fb51b6d38793b10579d7f72b0208e94
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2025-10-01 18:47:39 +0530

    Add files via upload

---
+++ 05_Dictionory/09_Character Counter.py
@@ -0,0 +1,24 @@
+def Character_Counter(s):
+    freq = {}
+
+    for ch in s:
+        if ch in freq:
+            freq[ch] += 1
+
+        else:
+            freq[ch] = 1
+
+    return freq
+
+s = "hello world"
+print(Character_Counter(s))
+
+# or
+
+from collections import Counter
+
+def Character_Counter_v2(s):
+    return Counter(s)
+
+s = "hello world"
+print(Character_Counter_v2(s))
