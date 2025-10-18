db13475c33ba6a8537025175f1f8a9f342831d97
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2025-10-18 07:10:46 +0530

    Add Dutch National Flag Algorithm implementation
    Implemented Dutch National Flag Algorithm for sorting an array of 0s, 1s, and 2s. Added a function to handle user input and sorting.

--- 01_Array or List/14_Dutch National Flag Algorithm.py
+++ 01_Array or List/14_Dutch National Flag Algorithm.py
@@ -1,3 +1,30 @@
+def sorting(arr):
+    low = 0
+    high = len(arr)-1
+    mid = 0
+
+    while mid <= high:
+        
+        if arr[mid] == 0:
+            arr[low],arr[mid] = arr[mid],arr[low]
+            low += 1
+            mid += 1
+
+        elif arr[mid] == 1:
+            mid += 1
+
+        else:
+            arr[mid],arr[high] = arr[high],arr[mid]
+            high -= 1
+
+    return arr
+
+arr = [2,1,0,2,1,0,0,1,2,0]
+
+print(sorting(arr))
+
+
+
 arr = [2,1,0,2,1,0,0,1,2,0]
 arr.sort()
 print(arr)
@@ -10,4 +37,5 @@ for i in range(N):
     arr.append(int(input()))
 
 for num in sorted(arr):
+
     print(num, end = " ")
