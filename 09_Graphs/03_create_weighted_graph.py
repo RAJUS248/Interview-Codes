9f5a36441c3ac8ec64d5871889fc6013bc4cd52e
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2025-10-19 19:25:41 +0530

    Update 03_create_weighted_graph.py

--- 09_Graphs/03_create_weighted_graph.py
+++ 09_Graphs/03_create_weighted_graph.py
@@ -40,4 +40,44 @@ for node in undirectional:
 
 print("\ndirectional weighted graph")
 for node in directional:
+
+    print(f"{node} --> {directional[node]}")
+
+
+# using set
+
+from collections import defaultdict
+
+def create_undirectional(edges):
+
+    graph = defaultdict(set)
+
+    for u,v in edges:
+        graph[u].add(v)
+        graph[v].add(u)
+
+    return graph
+
+
+def create_directional(edges):
+
+    graph = defaultdict(set)
+
+    for u,v in edges:
+        graph[u].add(v)
+
+    return graph
+       
+
+edges = [[0,1],[0,2],[1,3],[2,3]]
+undirectional = create_undirectional(edges)
+directional = create_directional(edges)
+
+print("\nundirectional graph")
+for node in undirectional:
+    print(f"{node} --> {undirectional[node]}")
+
+
+print("\ndirectional graph")
+for node in directional:
     print(f"{node} --> {directional[node]}")
