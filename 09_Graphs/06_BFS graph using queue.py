e7fc1526f07cf3071f6f0c66220e06fd65b12364
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2025-10-20 19:41:52 +0530

    Enhance BFS with adjacency list support
    Added BFS implementation for adjacency list and modified existing BFS function.

--- 09_Graphs/06_BFS graph using queue.py
+++ 09_Graphs/06_BFS graph using queue.py
@@ -38,3 +38,75 @@ print(graph)
 print("BFS")
 
 graph = bfs(graph,0)
+
+
+# mix both creating graph and also presented graph
+
+from collections import defaultdict,deque
+
+def create_graph(edges):
+
+    graph = defaultdict(list)
+
+    for u,v in edges:
+        graph[u].append(v)
+        graph[v].append(u)
+
+    return dict(graph)
+
+def bfs(graph,start):
+
+    visited = set()
+    queue = deque([start])
+    visited.add(start)
+    lst = []
+
+    while queue:
+        node = queue.popleft()
+        lst.append(node)
+
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+
+                visited.add(neighbor)
+                queue.append(neighbor)
+
+        
+    print(lst)
+
+# or for adj means already having graph like adj = [[2, 3, 1], [0], [0, 4], [0], [2]]
+
+def bfs_v2(adj):
+
+    V = len(adj)
+    visited = [False] * V
+    queue = deque([0])
+    visited[0] = True
+    lst = []
+
+    while queue:
+        node = queue.popleft()
+        lst.append(node)
+
+        for neighbor in adj[node]:
+            if  not visited[neighbor]:
+
+                visited[neighbor] = True
+                queue.append(neighbor)
+
+        
+    print(lst)
+
+edges = [[0,1],[0,2],[1,3],[2,3]]
+graph = create_graph(edges)
+print(graph)
+
+print("\nBFS")
+bfs(graph,0)
+
+
+print("\nBFS_v2")
+adj = [[2, 3, 1], [0], [0, 4], [0], [2]]
+bfs_v2(adj)
+
+
